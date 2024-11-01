bdf7aaa399d84781a3f62b57aa1c7482232dbd9b
MOW531 <76069060+MOW531@users.noreply.github.com>
2024-11-01 10:49:02 -0400

    Fixed error message.

--- Find_Rare_Items_Fix/hooks.py
+++ Find_Rare_Items_Fix/hooks.py
@@ -16,19 +16,20 @@ def on_container_open(
     __func: BoundFunction,
 ) -> None:
     pc = get_pc()
-    CurrentCom = pc.pawn.InvManager.GetCurrentCommDeck()
-    if (str(obj).find("VendingMachine")) == -1 and CurrentCom != None:
-        ComName = CurrentCom.ItemName
-        if (str(ComName).find("Scavenger")) > -1 or (str(ComName).find("Catalyst")) > -1:
-            OldAwesomeLevel = int(obj.GetAwesomeLevel())
-            AddAwesomeLevel = int(CurrentCom.ReplicatedAttributeSlotModifierValues[2])
-            obj.SetAwesomeLevel(OldAwesomeLevel + AddAwesomeLevel)
-            NewAwesomeLevel = int(obj.GetAwesomeLevel())
+    if pc.pawn != None:
+        CurrentCom = pc.pawn.InvManager.GetCurrentCommDeck()
+        if (str(obj).find("VendingMachine")) == -1 and CurrentCom != None:
+            ComName = CurrentCom.ItemName
+            if (str(ComName).find("Scavenger")) > -1 or (str(ComName).find("Catalyst")) > -1:
+                OldAwesomeLevel = int(obj.GetAwesomeLevel())
+                AddAwesomeLevel = int(CurrentCom.ReplicatedAttributeSlotModifierValues[2])
+                obj.SetAwesomeLevel(OldAwesomeLevel + AddAwesomeLevel)
+                NewAwesomeLevel = int(obj.GetAwesomeLevel())
 
-            print(obj)
-            print("Current Chest Awesome Level:", OldAwesomeLevel)
-            print("Adding:", AddAwesomeLevel)
-            print("New Chest Awesome Level:", NewAwesomeLevel)
+                print(obj)
+                print("Current Chest Awesome Level:", OldAwesomeLevel)
+                print("Adding:", AddAwesomeLevel)
+                print("New Chest Awesome Level:", NewAwesomeLevel)
 
 @hook(
     hook_func="WillowGame.WillowPawn:PostBeginPlay",
@@ -41,16 +42,17 @@ def on_enemy_spawn(
     __func: BoundFunction,
 ) -> None:
     pc = get_pc()
-    CurrentCom = pc.pawn.InvManager.GetCurrentCommDeck()
-    if obj.Class.Name != "WillowPlayerPawn" and CurrentCom != None:
-        ComName = CurrentCom.ItemName
-        if (str(ComName).find("Scavenger")) > -1 or (str(ComName).find("Catalyst")) > -1:
-            OldAwesomeLevel = int(obj.GetAwesomeLevel())
-            AddAwesomeLevel = int(CurrentCom.ReplicatedAttributeSlotModifierValues[2])
-            obj.SetAwesomeLevel(OldAwesomeLevel + AddAwesomeLevel)
-            NewAwesomeLevel = int(obj.GetAwesomeLevel())
+    if pc.pawn != None:
+        CurrentCom = pc.pawn.InvManager.GetCurrentCommDeck()
+        if obj.Class.Name != "WillowPlayerPawn" and CurrentCom != None:
+            ComName = CurrentCom.ItemName
+            if (str(ComName).find("Scavenger")) > -1 or (str(ComName).find("Catalyst")) > -1:
+                OldAwesomeLevel = int(obj.GetAwesomeLevel())
+                AddAwesomeLevel = int(CurrentCom.ReplicatedAttributeSlotModifierValues[2])
+                obj.SetAwesomeLevel(OldAwesomeLevel + AddAwesomeLevel)
+                NewAwesomeLevel = int(obj.GetAwesomeLevel())
 
-            print(obj)
-            print("Current Enemy Awesome Level:", OldAwesomeLevel)
-            print("Adding:", AddAwesomeLevel)
-            print("New Enemy Awesome Level:", NewAwesomeLevel)
+                print(obj)
+                print("Current Enemy Awesome Level:", OldAwesomeLevel)
+                print("Adding:", AddAwesomeLevel)
+                print("New Enemy Awesome Level:", NewAwesomeLevel)
