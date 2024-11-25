15c72a61b7b0a4366fdba1b4b55eb35247807662
MOW531 <76069060+MOW531@users.noreply.github.com>
2024-11-25 17:29:34 -0500

    Add files via upload

--- SDU Guaranteed/Binaries/Mods/SDUGuaranteed/__init__.py
+++ SDU Guaranteed/Binaries/Mods/SDUGuaranteed/__init__.py
@@ -3,17 +3,9 @@ import unrealsdk
 from pathlib import Path
 from unrealsdk.hooks import Type
 from unrealsdk.unreal import UObject, WrappedStruct, BoundFunction
-from mods_base import hook, get_pc 
-from mods_base.options import BaseOption, BoolOption
-from mods_base import SETTINGS_DIR
-from mods_base import build_mod
+from mods_base import hook, SETTINGS_DIR, build_mod
 from unrealsdk import logging
-import os
 
-TEXT_MOD_FOLDER = "./Mods/BadTextModLoader/TextMods/"
-
-setcommands = []
-rsetcommands = []
 bPatched = False
 bPatched_volatile = False
 Count = 0
@@ -30,12 +22,21 @@ def obj (definition:str, object:str):
     return unrealsdk.find_object(definition, object)
 
 
-def patch():
-    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").ObjectFlags |= 0x4000
-    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers = obj("InventoryBalanceDefinition","SDUGuaranteed.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers
-
 def patch_volatile():
-    a=1
+    # Base Game
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers = [struct("InventoryManufacturerBalanceData", Manufacturer=obj("ManufacturerDefinition","gd_manufacturers.Manufacturers.Dahl"), Grades=[struct("InventoryGameStageGradeWeightData", GradeModifiers=struct("InventoryGradeModifierData", ExpLevel=1, CustomInventoryDefinition=obj("ItemDefinition","gd_StorageDeckUpgrade.INV_InventorySpace.INV_InventorySpace")))])]
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].GameStageRequirement.MinGameStage = 1
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].GameStageRequirement.MaxGameStage = 100
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].MinSpawnProbabilityModifier.BaseValueConstant = 1
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].MinSpawnProbabilityModifier.BaseValueScaleConstant = 1
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].MaxSpawnProbabilityModifier.BaseValueConstant = 1
+    obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers[0].Grades[0].MaxSpawnProbabilityModifier.BaseValueScaleConstant = 1
+
+    # DLC
+    obj("InventoryBalanceDefinition","dlc1_gd_itemgrades.StorageDeckUpgrades.dlc1_ItemGrade_SDU_InventorySlots").Manufacturers = obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers
+    obj("InventoryBalanceDefinition","DLC4_Itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers = obj("InventoryBalanceDefinition","gd_itemgrades.StorageDeckUpgrades.ItemGrade_SDU_InventorySlots").Manufacturers
+
+
 
 @hook(
     hook_func="Engine.WorldInfo:CommitMapChange",
@@ -67,29 +68,8 @@ def on_level_loaded(
         if bPatched_volatile != True:
             bPatched_volatile = True
             patch_volatile()
-            #if len(unrealsdk.find_all("WorldInfo")) > 1:
-            #        if unrealsdk.find_all("WorldInfo")[1].GetMapName() == "scrap_trashcoast_p":
-            #            unrealsdk.find_object("WillowAIPawn","scrap_trash_coast_p.TheWorld:PersistentLevel.WillowAIPawn_0").ActorSpawnCost = 0
-            #            print("Skrappy Patched!")
     else:
         Count = Count + 1
-
-@hook(
-    hook_func="Engine.WorldInfo:IsMenuLevel",
-    hook_type=Type.PRE,
-)
-def on_startgame(
-    obj: UObject,
-    __args: WrappedStruct,
-    __ret: any,
-    __func: BoundFunction,
-) -> None:
-    global bPatched
-    if bPatched is False:
-        bPatched = True
-        patch()
-
-
 
 
 
@@ -104,7 +84,7 @@ build_mod(
     # version_info_parser=lambda v: tuple(int(x) for x in v.split(".")),
     # deregister_same_settings=True,      # This is True by default
     keybinds=[],
-    hooks=[on_startgame],
+    hooks=[on_level_loaded, on_commit_map_change],
     commands=[],
     # Defaults to f"{SETTINGS_DIR}/dir_name.json" i.e., ./Settings/bl1_commander.json
     settings_file=Path(f"{SETTINGS_DIR}/SDUGuaranteed.json"),
